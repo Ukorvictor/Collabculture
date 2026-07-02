02a0d47ea8f95ee2e8dc0c94053ad3d13c97e318
Victor Ukor <ukorvictor@gmail.com>
2026-07-02 16:20:39 -0400

    Setup deployment

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,5 +3,12 @@ const config = {
     "@tailwindcss/postcss": {},
   },
 };
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
 
+module.exports = nextConfig;
 export default config;
